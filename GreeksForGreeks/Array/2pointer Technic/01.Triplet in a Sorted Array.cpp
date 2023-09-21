87ddf2f92ab372e444d66d0b7301fa6058208765
chanchalKhatua <142116201+chanchalKhatua@users.noreply.github.com>
2023-09-21 19:04:21 +0530

    Update 01.Triplet in a Sorted Array.cpp

--- GreeksForGreeks/Array/2pointer Technic/01.Triplet in a Sorted Array.cpp
+++ GreeksForGreeks/Array/2pointer Technic/01.Triplet in a Sorted Array.cpp
@@ -12,4 +12,34 @@
 // Explanation:
 // The triplet {1, 4, 8} in 
 // the array sums up to 13.
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        
+        sort(A,A+n);
+        for(int i=0;i<n-2;i++)
+        {
+            int sum=X;
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+                if(A[i]+A[j]+A[k]==sum)
+                return true;
+                else if(A[i]+A[j]+A[k]>sum)
+                {
+                    k--;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+        }
+        return false;
+    }
 
+};
