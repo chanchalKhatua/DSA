6cc6866cdb4e41eecf86accdf80988fcf83e507f
chanchalKhatua <142116201+chanchalKhatua@users.noreply.github.com>
2023-10-05 14:01:18 +0530

    Update 1.Kth smallest element.cpp

--- GreeksForGreeks/PriorityQueue/1.Kth smallest element.cpp
+++ GreeksForGreeks/PriorityQueue/1.Kth smallest element.cpp
@@ -14,6 +14,7 @@ class Solution{
     // l : starting index of the array i.e 0
     // r : ending index of the array i.e size-1
     // k : find kth smallest element and return using this function
+//using priority solution;
     int kthSmallest(int arr[], int l, int r, int k) {
         //code here
         priority_queue<int> pq;
@@ -31,6 +32,65 @@ class Solution{
         return pq.top();
     }
 };
+// using quick sort algoritham
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+ int partition(int arr[], int l, int r)
+{
+    int x = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++) {
+        if (arr[j] <= x) {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+ 
+    swap(arr[i], arr[r]);
+    return i;
+}
+
+ 
+// This function returns K'th smallest element in arr[l..r]
+// using QuickSort based method. ASSUMPTION: ALL ELEMENTS IN
+// ARR[] ARE DISTINCT
+int kthSmallest(int arr[], int l, int r, int K)
+{
+    // If k is smaller than number of elements in array
+    if (K > 0 && K <= r - l + 1) {
+ 
+        // Partition the array around last element and get
+        // position of pivot element in sorted array
+        int pos = partition(arr, l, r);
+ 
+        // If position is same as k
+        if (pos - l == K - 1)
+            return arr[pos];
+        if (pos - l > K - 1) // If position is more, recur
+                             // for left subarray
+            return kthSmallest(arr, l, pos - 1, K);
+ 
+        // Else recur for right subarray
+        return kthSmallest(arr, pos + 1, r,
+                           K - pos + l - 1);
+    }
+ 
+    // If k is more than number of elements in array
+    return INT_MAX;
+}
+};
+ 
+
+// Standard partition process of QuickSort(). It considers
+// the last element as pivot and moves all smaller element
+// to left of it and greater elements to right
+
+
 
 
 //{ Driver Code Starts.
