879aab0e854b8b49511d3f22c71c71078f2ea2a3
chanchalKhatua <142116201+chanchalKhatua@users.noreply.github.com>
2023-10-23 17:01:34 +0530

    Create 01.Naive Approach.cpp

---
+++ GreeksForGreeks/String/Pattern Searching/01.Naive Approach.cpp
@@ -0,0 +1,13 @@
+void patSearchinng(string &txt,string &pat){
+    int m=pat.length();
+    int n=txt.length();
+    for(int i=0;i<=(n-m);i++){
+        int j;
+        for(j=0;j<m;j++)
+        if(pat[j]!=txt[i+j])
+        break;
+        
+        if(j==m)
+        cout<<i<<" ";
+    }
+}
